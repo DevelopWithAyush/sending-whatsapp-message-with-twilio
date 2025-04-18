2041dba85bae23356ab67b0446eefc9ab5498244
DevelopWithAyush <developwithdubey@gmail.com>
2025-04-18 12:01:37 +0530

    adding base url

--- swagger/swaggerSpec.js
+++ swagger/swaggerSpec.js
@@ -17,7 +17,7 @@ const options = {
         },
         servers: [
             {
-                url: `http://localhost:${process.env.PORT || 5000}`,
+                url: `${process.env.BASE_URL}`,
                 description: 'Development server',
             },
         ],
